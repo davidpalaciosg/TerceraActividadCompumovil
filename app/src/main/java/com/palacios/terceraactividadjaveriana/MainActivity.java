d6024aa8fa1ddca84e4f4cb296697b004bbf99a9
Aulas Ingeniería <aulasingenieria@javeriana.edu.co>
2022-05-06 10:06:09 -0500

    Prueba maps

--- app/src/main/java/com/palacios/terceraactividadjaveriana/MainActivity.java
+++ app/src/main/java/com/palacios/terceraactividadjaveriana/MainActivity.java
@@ -2,13 +2,30 @@ package com.palacios.terceraactividadjaveriana;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Inflate
+        boton = findViewById(R.id.boton);
+
+
+        //Click on boton
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
