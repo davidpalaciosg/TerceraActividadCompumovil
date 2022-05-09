225dd4c78148d155f02ffa76ec0533918be66aeb
David Enrique Palacios Garcia <paladavid@hotmail.com>
2022-05-09 12:19:35 -0500

    Files and Camera feature done

--- app/src/main/java/com/palacios/terceraactividadjaveriana/SignUpActivity.java
+++ app/src/main/java/com/palacios/terceraactividadjaveriana/SignUpActivity.java
@@ -1,13 +1,25 @@
 package com.palacios.terceraactividadjaveriana;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class SignUpActivity extends AppCompatActivity {
     //Firebase
@@ -22,6 +34,9 @@ public class SignUpActivity extends AppCompatActivity {
     private Button btnCameraSign;
     private Button btnFilesSign;
     private Button btnSaveSign;
+
+    //Uri to store the camera picture
+    Uri uriCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +56,88 @@ public class SignUpActivity extends AppCompatActivity {
         btnFilesSign = findViewById(R.id.btnFilesSign);
         btnSaveSign = findViewById(R.id.btnSaveSign);
 
+        startButtons();
+
 
     }
+
+
+    private void startButtons() {
+        //Gallery
+        btnFilesSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGetContentGallery.launch("image/*");
+            }
+        });
+        //Camera
+        btnCameraSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startCamera();
+            }
+        });
+        btnSaveSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //---------------------------------------------------------------CAMERA AND FILES---------------------------------------------------
+    //Starts camera and the photo taken is saved on uriCamera
+    private void startCamera(){
+        File file = new File(getFilesDir(), "picFromCamera");
+        uriCamera = FileProvider.getUriForFile(this,
+                getApplicationContext().getPackageName() + ".fileprovider", file);
+        mGetContentCamera.launch(uriCamera);
+    }
+
+
+    //Open gallery and get its uri
+    ActivityResultLauncher<String> mGetContentGallery = registerForActivityResult(
+            new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uriLocal) {
+                    //Load image on a view…
+                    setImage(uriLocal);
+                }
+            });
+
+    ActivityResultLauncher<Uri> mGetContentCamera =
+            registerForActivityResult(new ActivityResultContracts.TakePicture(),
+                    new ActivityResultCallback<Boolean>() {
+                        @Override
+                        public void onActivityResult(Boolean result) {
+                            //Load image on a view
+                            setImage(uriCamera);
+                        }
+                    });
+
+
+    private void setImage(Uri uri) {
+        InputStream imageStream = null;
+        try {
+            imageStream = getContentResolver().openInputStream(uri);
+            final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+            imgSign.setImageBitmap(selectedImage);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
